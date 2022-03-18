feefe895676bff25f4f6018b54b36959129aba83
LÊ TẤN PHƯƠNG SANG <4501104198@student.hcmue.edu.vn>
2022-03-18 20:17:21 +0700

    A0-1: Create Form Login

---
+++ Agile09/Agile09/Common/Constant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agile09.Common
+{
+    public class Constant
+    {
+        public static string USER_SECTION = "USER_SECTION";
+        public static string ADMIN_SECTION = "ADMIN_SECTION";
+    }
+}
