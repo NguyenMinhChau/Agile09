47ed6d8c9f813f5bdd45c1ef98adae6a4f4a6173
LÊ TẤN PHƯƠNG SANG <4501104198@student.hcmue.edu.vn>
2022-03-18 21:53:47 +0700

    A0-1: Create Form Login

--- Agile09/Agile09/App_Start/RouteConfig.cs
+++ Agile09/Agile09/App_Start/RouteConfig.cs
@@ -14,9 +14,64 @@ namespace Agile09
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Product Category",
+                url: "san-pham/{MetaTitle}-{id}",
+                defaults: new { controller = "Home", action = "Category", id = UrlParameter.Optional, },
+                namespaces: new[] { "Agile09.Controllers" }
+            );
+            routes.MapRoute(
+               name: "Payment",
+               url: "thanh-toan",
+               defaults: new { controller = "CartItem", action = "Payment", id = UrlParameter.Optional, },
+               namespaces: new[] { "Agile09.Controllers" }
+           );
+            routes.MapRoute(
+               name: "Cart",
+               url: "gio-hang",
+               defaults: new { controller = "CartItem", action = "Index", id = UrlParameter.Optional, },
+               namespaces: new[] { "Agile09.Controllers" }
+           );
+            routes.MapRoute(
+               name: "Success",
+               url: "hoan-thanh",
+               defaults: new { controller = "CartItem", action = "Success", id = UrlParameter.Optional, },
+               namespaces: new[] { "Agile09.Controllers" }
+           );
+            routes.MapRoute(
+               name: "Error",
+               url: "loi-thanh-toan",
+               defaults: new { controller = "CartItem", action = "Error", id = UrlParameter.Optional, },
+               namespaces: new[] { "Agile09.Controllers" }
+           );
+            routes.MapRoute(
+               name: "Contact",
+               url: "lien-he",
+               defaults: new { controller = "Contact", action = "Index", id = UrlParameter.Optional, },
+               namespaces: new[] { "Agile09.Controllers" }
+           );
+            routes.MapRoute(
+               name: "Add Cart",
+               url: "them-gio-hang",
+               defaults: new { controller = "CartItem", action = "AddItem", id = UrlParameter.Optional, },
+               namespaces: new[] { "Agile09.Controllers" }
+           );
+            routes.MapRoute(
+                name: "Product Detail",
+                url: "chi-tiet/{MetaTitle}-{id}",
+                defaults: new { controller = "Home", action = "Detail", id = UrlParameter.Optional, },
+                namespaces: new[] { "Agile09.Controllers" }
+            );
+            routes.MapRoute(
+                name: "About",
+                url: "gioi-thieu/",
+                defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional, },
+                namespaces: new[] { "Agile09.Controllers" }
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, },
+                namespaces: new[] { "Agile09.Controllers" }
             );
         }
     }
