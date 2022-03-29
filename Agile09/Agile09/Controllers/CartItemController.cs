7974e87c2e85b60c2021b5d9b9ebe1cbad58b9de
HỒ HÀN THIÊN <92928215+todo-lists-thien@users.noreply.github.com>
2022-03-29 19:38:56 +0700

    A0-32 : add-product-to-cart

--- Agile09/Agile09/Controllers/CartItemController.cs
+++ Agile09/Agile09/Controllers/CartItemController.cs
@@ -24,5 +24,47 @@ namespace Agile09.Controllers
             }
             return View(list);
         }
+        public ActionResult AddItem(long productID, int quantity)
+        {
+            var product = new ProductDAO().ViewDetail(productID);
+            var cart = Session[CommonConstants.CartSession];
+            if (cart != null)
+            {
+                var list = (List<CartItem>)cart;
+                if (list.Exists(x => x.Product.ID == productID))
+                {
+                    foreach (var item in list)
+                    {
+                        if (item.Product.ID == productID)
+                        {
+                            item.Quantity += quantity;
+                        }
+                    }
+                }
+                else
+                {
+                    //tạo mới đối tượng cart item
+                    var item = new CartItem();
+                    item.Product = product;
+                    item.Quantity = quantity;
+                    list.Add(item);
+
+                }
+                //gán vào sesstion
+                Session[CommonConstants.CartSession] = list;
+            }
+            else
+            {
+                //tạo mới đối tượng cart item
+                var item = new CartItem();
+                item.Product = product;
+                item.Quantity = quantity;
+                var list = new List<CartItem>();
+                //gán vào sesstion
+                list.Add(item);
+                Session[CommonConstants.CartSession] = list;
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
