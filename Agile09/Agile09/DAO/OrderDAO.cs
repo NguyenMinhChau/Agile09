4b8c48ba172a20ac105d057a73dc66d309383c15
LÊ QUANG HƯNG <4501104099@student.hcmue.edu.vn>
2022-04-01 20:10:52 +0700

    A0-35: Update products from cart

---
+++ Agile09/Agile09/DAO/OrderDAO.cs
@@ -0,0 +1,41 @@
+﻿using Agile09.EF;
+using Agile09.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Agile09.DAO
+{
+    public class OrderDAO
+    {
+        ShopOnlineDb db = null;
+        public OrderDAO()
+        {
+            db = new ShopOnlineDb();
+        }
+        public long Insert(OrderProduct order)
+        {
+            db.OrderProducts.Add(order);
+            db.SaveChanges();
+            return order.ID;
+        }
+        public IEnumerable<RevunesVM> ListAll(DateTime? fromDate, DateTime? toDate)
+        {
+            var paramsmeter = new SqlParameter[] {
+                new SqlParameter("@fromDate", fromDate),
+                new SqlParameter("@toDate", toDate)
+            };
+            return db.Database.SqlQuery<RevunesVM>("proc_ThongKe @fromDate,@toDate", paramsmeter);
+        }
+        public IEnumerable<RevenusSP> ThongKeSP(DateTime? fromDate, DateTime? toDate)
+        {
+            var paramsmeter = new SqlParameter[] {
+                new SqlParameter("@fromDate", fromDate),
+                new SqlParameter("@toDate", toDate)
+            };
+            return db.Database.SqlQuery<RevenusSP>("pro_ThongKeSP @fromDate,@toDate", paramsmeter);
+        }
+    }
+}
