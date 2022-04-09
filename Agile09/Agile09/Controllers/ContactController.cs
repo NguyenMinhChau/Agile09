c0829fb94a7ca6da2c84600dc94fba758d112aa5
Admin <Admin@DESKTOP-8DR9P3R>
2022-04-09 20:17:40 +0700

    A0-37: send feedback and comments

---
+++ Agile09/Agile09/Controllers/ContactController.cs
@@ -0,0 +1,47 @@
+﻿using Agile09.DAO;
+using Agile09.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Agile09.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var model = new ContactDAO().GetActiveContext();
+            return View(model);
+        }
+        [HttpPost]
+        public JsonResult Send(string name, string phone, string email, string content, string address)
+        {
+            var feedback = new Feedback();
+            feedback.Name = name;
+            feedback.Phone = phone;
+            feedback.Email = email;
+            feedback.Content = content;
+            feedback.Address = address;
+            feedback.CreatedDate = DateTime.Now;
+
+            var id = new ContactDAO().InsertFeedBack(feedback);
+            if (id > 0&& feedback.Name !=""&& feedback.Phone!=""&& feedback.Email!=""&&feedback.Content !=""&& feedback.Address!="")
+            {
+                return Json(new
+                {
+                    status = true
+                });
+            }
+            else
+            {
+                return Json(new
+                {
+                    status = false
+                });
+            }
+        }
+    }
+}
---
+++ Agile09/Agile09/DAO/ContactDAO.cs
@@ -0,0 +1,29 @@
+﻿using Agile09.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agile09.DAO
+{
+    public class ContactDAO
+    {
+        ShopOnlineDb db = null;
+        public ContactDAO()
+        {
+            db = new ShopOnlineDb();
+        }
+        public Contact GetActiveContext()
+        {
+            return db.Contacts.Single(x => x.Status == true);
+        }
+        public int InsertFeedBack(Feedback fb)
+        {
+            db.Feedbacks.Add(fb);
+            db.SaveChanges();
+            return fb.ID;
+        }
+
+    }
+
+}
